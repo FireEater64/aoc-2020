8331d66a7133313dc06a27a929511623fd815818
George Vanburgh <george@vanburgh.me>
2020-12-06 11:49:41 +0000

    Day 06
    This time with more HashSet<T>

--- src/day06/Program.cs
+++ src/day06/Program.cs
@@ -1,16 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-var groups = File.ReadAllText("input.txt")
+var passengerGroups = File.ReadAllText("input.txt")
     .Split($"{Environment.NewLine}{Environment.NewLine}")
-    .Select(str => str.Replace(Environment.NewLine, "")
-        .GroupBy(ch => ch)
-        .Select(group => new { GroupSize = str.Count(c => c == '\n') + 1, Answered = group }
-    ));
+    .Select(str => str.Split(Environment.NewLine).Select(line => line.ToHashSet()));
 
-var result = groups.Sum(group => group.Count(c => c.Answered.Count() > 0));
-var result2 = groups.Sum(group => group.Count(c => c.Answered.Count() == c.GroupSize));
+var unions = passengerGroups.Select(group => UnionAll(group)).Sum(set => set.Count);
+var intersections = passengerGroups.Select(group => IntersectAll(group)).Sum(set => set.Count);
 
-Console.WriteLine($"Part 1: {result}");
-Console.WriteLine($"Part 2: {result2}");
+Console.WriteLine($"Part 1: {unions}");
+Console.WriteLine($"Part 2: {intersections}");
+
+HashSet<T> UnionAll<T>(IEnumerable<HashSet<T>> sets) => sets.Any() ? sets.Aggregate(new HashSet<T>(sets.First()), (result, next) => { result.UnionWith(next); return result; }) : new HashSet<T>();
+HashSet<T> IntersectAll<T>(IEnumerable<HashSet<T>> sets) => sets.Any() ? sets.Aggregate(new HashSet<T>(sets.First()), (result, next) => { result.IntersectWith(next); return result; }) : new HashSet<T>();
