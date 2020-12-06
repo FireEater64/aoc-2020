f98b20447fff414a9d8a3e3abccf006ec7e753e0
George Vanburgh <george@vanburgh.me>
2020-12-06 12:42:34 +0000

    Improvements for Day 04
    Recursive patterns <3

--- src/day04/Program.cs
+++ src/day04/Program.cs
@@ -18,7 +18,7 @@ record Passport
     public int? BirthYear { get; init; }
     public int? IssueYear { get; init; }
     public int? ExpirationYear { get; init; }
-    public Height? Height { get; init; }
+    public string? Height { get; init; }
     public string? HairColour { get; init; }
     public string? EyeColour { get; init; }
     public string? PassportId { get; init; }
@@ -30,7 +30,7 @@ record Passport
         BirthYear = kvp.TryGetValue("byr", out var byr) ? int.Parse(byr) : null;
         IssueYear = kvp.TryGetValue("iyr", out var iyr) ? int.Parse(iyr) : null;
         ExpirationYear = kvp.TryGetValue("eyr", out var eyr) ? int.Parse(eyr) : null;
-        Height = kvp.TryGetValue("hgt", out var hgt) ? new Height(hgt) : null;
+        Height = kvp.TryGetValue("hgt", out var hgt) ? hgt : null;
         HairColour = kvp.TryGetValue("hcl", out var hcl) ? hcl : null;
         EyeColour = kvp.TryGetValue("ecl", out var ecl) ? ecl : null;
         PassportId = kvp.TryGetValue("pid", out var pid) ? pid : null;
@@ -41,33 +41,20 @@ record Passport
     public bool IsValidPart1 => this switch
     {
         Passport { NumFields: 8 } => true,
-        Passport { NumFields: 7 } p => !p.CountryId.HasValue,
+        Passport { NumFields: 7, CountryId: null } => true,
         _ => false
     };
 
-    public bool IsValidPart2 => BirthYear is (>= 1000 and <= 2002) &&
-                                IssueYear is (>= 2010 and <= 2020) &&
-                                ExpirationYear is (>= 2020 and <= 2030) &&
-                                Height is not null && Height.IsValid &&
+    public bool IsValidPart2 => BirthYear is >= 1000 and <= 2002 &&
+                                IssueYear is >= 2010 and <= 2020 &&
+                                ExpirationYear is >= 2020 and <= 2030 &&
                                 HairColour is not null && HairColour.StartsWith("#") && HairColour.Length == 7 &&
-                                EyeColour is ("amb" or "blu" or "brn" or "gry" or "grn" or "hzl" or "oth") &&
-                                PassportId is not null && PassportId.Length == 9;
-}
-
-record Height
-{
-    int Amount { get; init; }
-    string Unit { get; set; }
-    public Height(string s)
-    {
-        Amount = int.Parse(new string(s.TakeWhile(c => char.IsDigit(c)).ToArray()));
-        Unit = s.EndsWith("cm") ? "cm" : "in";
-    }
-
-    public bool IsValid => this switch
-    {
-        Height { Unit: "cm" } h when h.Amount is (>= 150 and <= 193) => true,
-        Height { Unit: "in" } h when h.Amount is (>= 59 and <= 76) => true,
-        _ => false
-    };
+                                EyeColour is "amb" or "blu" or "brn" or "gry" or "grn" or "hzl" or "oth" &&
+                                PassportId is not null && PassportId.Length == 9 &&
+                                Height switch 
+                                {
+                                    string h when h.EndsWith("cm") && int.Parse(h[..^2]) is >= 150 and <= 193 => true,
+                                    string h when h.EndsWith("in") && int.Parse(h[..^2]) is >= 59 and <= 76 => true,
+                                    _ => false,
+                                };
 }
